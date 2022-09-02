75848223398f2fab15e3525cc229b89d20ef77cd
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2022-09-01 23:19:21 -0300

    Create PrimeiroPrograma.java

---
+++ Material didático/Curso em vídeo/02-/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+package primeiroprograma;   /* Pacote criado */
+
+public class PrimeiroPrograma {     /* Classe */
+
+   
+    public static void main(String[] args) {    /* método principal do pacote */
+        
+    }
+    
+}
