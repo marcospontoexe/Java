c82c5954fdabe96d00ffe3be2dc5449b771c6c55
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2022-09-04 12:13:00 -0300

    primeiro programa

---
+++ Material didático/Curso em vídeo/02-/PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+package primeiroprograma;       /* nome do pacote */
+
+public class PrimeiroPrograma {      /* nome da classe */
+
+    public static void main(String[] args) {        /* método principal do pacote */
+         System.out.print("Olá mundo!!!");
+    }
+    
+}
