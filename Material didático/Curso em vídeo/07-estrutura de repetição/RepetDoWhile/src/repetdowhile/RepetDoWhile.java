fe66a793526dabff404fc46bde58b9cae0700376
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2022-09-13 20:48:48 -0300

    estrutura de repetição do while

---
+++ Material didático/Curso em vídeo/07-estrutura de repetição/RepetDoWhile/src/repetdowhile/RepetDoWhile.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package repetdowhile;
+
+/**
+ *
+ * @author marcos
+ */
+public class RepetDoWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int cont = 0;
+        
+        do{
+            cont++;
+            System.out.println("cont: " + cont);
+        }
+        while(cont < 10);
+    }
+    
+}
