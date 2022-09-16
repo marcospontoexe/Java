5d94b8cfd3b989ba12af277031fb42730e4dcfbe
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2022-09-15 23:03:14 -0300

    criando objetos

---
+++ Material didático/Curso em vídeo/09-métodos/Objetos/src/objetos/Fatorial.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package objetos;
+
+/**
+ *
+ * @author marcos
+ */
+public class Fatorial {
+    //DECLARAÇÃO DOS ATRIBUTOS
+    private int num = 0;        //private torna o acesso dos atributos disponíveis apenas dentro da classe pertecentes
+    private int fat = 1;
+    private String formula ="";
+    
+    //DECLARAÇÃO E MÉTODOS
+    public void setValor(int n){    //método de alterar um atributo privado
+        num = n;        //método de alterar um atributo privado
+        int f = 1;
+        String temp = "";
+        for(int i = n; i>1; i--){
+            f *= i;
+            temp += i + "*";
+        }
+        temp += "1 = ";
+        fat = f;
+        formula = temp;
+        
+    }
+    
+    public int getFatorial(){
+        return fat;
+    }
+    
+    public String getFormula(){
+        return formula;
+    }
+    
+    
+}
---
+++ Material didático/Curso em vídeo/09-métodos/Objetos/src/objetos/Objetos.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package objetos;
+
+/**
+ *
+ * @author marcos
+ */
+public class Objetos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Fatorial f = new Fatorial();    
+        System.out.println("Fatorial de cinco;");
+        f.setValor(5);
+        System.out.print(f.getFormula());
+        System.out.println(f.getFatorial());
+        
+        
+    }
+    
+}
