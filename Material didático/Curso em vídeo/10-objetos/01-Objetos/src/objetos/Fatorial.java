1b7f7a363ab7039c722e3de2edfa382647eb7490
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2022-09-18 13:47:28 -0300

    atualização de conteúdo de classes

--- Material didático/Curso em vídeo/10-objetos/01-Objetos/src/objetos/Fatorial.java
+++ Material didático/Curso em vídeo/10-objetos/01-Objetos/src/objetos/Fatorial.java
@@ -10,9 +10,16 @@ package objetos;
  */
 public class Fatorial {
     //DECLARAÇÃO DOS ATRIBUTOS
-    private int num = 0;        //private torna o acesso dos atributos disponíveis apenas dentro da classe pertecentes
-    private int fat = 1;
-    private String formula ="";
+    private int num = 0;        //modificador de visibilidade: public(-), private(+), protect(#)
+    private int fat = 1;        //Indica o níveis de acesso aos atributos e métodos de uma classe
+    private String formula =""; //private torna o acesso dos atributos disponíveis apenas dentro da classe pertecentes
+    
+    /* MODIFICADORES DE ACESSO
+    Indica o níveis de acesso aos atributos e métodos de uma classe;
+    public(-): Qualquer classe tem acesso aos atributos e métodos
+    private(+): Apenas a classe proprietária do atributos ou método tem acesso
+    protect(#): Classes mães e filhas podem ter acesso aos atributos e métodos
+    */
     
     //DECLARAÇÃO E MÉTODOS
     //define o estado dos atributos
