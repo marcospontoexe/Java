2b658de181dd4c2717f039bb46e6f7f8c6e26701
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2022-09-28 22:26:07 -0300

    polimorfismo por sobrecarga

--- Material didático/Curso em vídeo/10-objetos/05-polimorfismo/SobreCarga/src/sobrecarga/SobreCarga.java
+++ Material didático/Curso em vídeo/10-objetos/05-polimorfismo/SobreCarga/src/sobrecarga/SobreCarga.java
@@ -1,11 +1,23 @@
 package sobrecarga;
 /*
-POLIMORFISMO POR SOBRECARGA: A assinatura do método é diferente
+POLIMORFISMO POR SOBRECARGA: A assinatura do método é diferente, e precisa estar na mesma classe
 */
 
 public class SobreCarga {
     public static void main(String[] args) {
+        Cachorro c = new Cachorro();
         
+        c.reagir("toma comida");    //abanar rabo e latir
+        c.reagir("pra casinha");    //rosnar
+        
+        c.reagir(11,45);        //Abanar o rabo
+        c.reagir(21,00);        //Abanar e latir
+        
+        c.reagir(false);        //Rosnar e latir
+        c.reagir(true);         //Abanar o rabo
+        
+        c.reagir(2, 12.5f);     //Latir
+        c.reagir(17, 4.5f);     //Rosnar
     }
     
 }
