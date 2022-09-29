2ef85f3010c7f9e4f7617eb1ded1b38e785ee2d4
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2022-09-28 21:56:40 -0300

    polimorfismo por sobre carga

---
+++ Material didático/Curso em vídeo/10-objetos/05-polimorfismo/SobreCarga/src/sobrecarga/SobreCarga.java
@@ -0,0 +1,11 @@
+package sobrecarga;
+/*
+POLIMORFISMO POR SOBRECARGA: A assinatura do método é diferente
+*/
+
+public class SobreCarga {
+    public static void main(String[] args) {
+        
+    }
+    
+}
